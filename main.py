98c63683c4763c9c102745aaff7d6fac3d299027
SpyralTraifes <partitiondoux@protonmail.com>
2025-05-21 22:20:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 import redis.asyncio as redis
